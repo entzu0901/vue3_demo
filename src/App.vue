5ad09b0b488ea2ab2aba30f75d6c3149eb58fcb1
entzu0901 <lamigo079@gmail.com>
2022-01-17 20:33:10 +0800

    setup的兩個注意點

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,22 @@
 <template>
-  <h1>一個人的訊息</h1>
-  <h2>姓名:{{name}}</h2>
-  <h2>年齡:{{age}}</h2>
-  <h2>工作種類:{{job.type}}</h2>
-  <h2>工作薪水:{{job.salary}}</h2>
-  <button @click="change">修改人的訊息</button>
+  <demo @hello="showHelloMsg" msg="你好啊" school='blibli'>
+    <template v-slot:schoolName>  
+      <span>blibli</span>
+    </template>
+  </demo>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import demo from './components/demo.vue'
 export default {
-  name: 'App',
+  components: { demo },
+  name:'app',
   setup(){
-    let name=ref('張三')
-    let age=ref(20)
-    let job=ref({
-      type:"前端工程師",
-      salary:'30k'
-    })
-    function change() {
-     name.value="李四",
-     age.value=30,
-     job.value.type='UI設計師',
-     job.value.salary="55k"
+    function showHelloMsg(value){
+      alert(`你好啊:${value}`)
     }
-    return{
-      name,
-      age,
-      change,
-      job
-    }
-      // return ()=> h('h1','blibli')
+    return{showHelloMsg}
   }
 }
 </script>
 
-<style>
-
-</style>

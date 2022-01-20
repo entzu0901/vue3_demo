c116dbd949f30f81ffc34282ce98f6761897e260
entzu0901 <lamigo079@gmail.com>
2022-01-20 12:46:42 +0800

    new

---
+++ src -自訂義hook/components/demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>當前求和為{{sum}}</h2>
+  <button @click="sum++">點我+1</button>
+  <hr>
+  <h2>當前點擊的滑鼠座標為x:{{point.x}},y:{{point.y}}</h2>
+</template>
+
+<script>
+import {ref} from 'vue'
+import usePoint from '../hook/usepoint'
+export default {
+  name: 'demo',
+  setup(){
+    let sum=ref(0)
+    let point=usePoint()
+   return{sum,point}
+  }
+}
+</script>
